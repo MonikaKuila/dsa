25171af910a76841cfef9e25ddd1fd8d0ad5907a
monikakuila <mkuila242@gmail.com>
2026-01-15 16:11:19 +0530

    #left_rotate_by_1_place

--- src/Arrays/Easy/Revision/left_rotate_1_place.java
+++ src/Arrays/Easy/Revision/left_rotate_1_place.java
@@ -1,4 +1,30 @@
 package Arrays.Easy.Revision;
 
-public class left_rotate_1_place {
+public class left_rotate_1_place
+{
+    public static int[] left_rotate(int[] arr)
+    {
+        int left = arr[0];
+       for(int i=1;i<arr.length;i++)
+       {
+          arr[i-1] = arr[i];
+       }
+
+       arr[arr.length - 1]=left;
+
+       return arr;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = {1,2,3,4,5};
+        arr = left_rotate(arr);
+        System.out.println("Rotated array is");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]);
+        }
+
+
+    }
 }
