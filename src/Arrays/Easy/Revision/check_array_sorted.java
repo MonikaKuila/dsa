9b691490f52edb59f0e6285713b354b4061ad1a4
monikakuila <mkuila242@gmail.com>
2026-01-15 11:26:08 +0530

    #check_array_sorted_or_not

--- src/Arrays/Easy/Revision/check_array_sorted.java
+++ src/Arrays/Easy/Revision/check_array_sorted.java
@@ -1,4 +1,46 @@
 package Arrays.Easy.Revision;
 
-public class check_array_sorted {
+public class check_array_sorted
+{
+    public static boolean check_arr_sorted(int[] arr)
+    {
+
+       for(int i=1;i<arr.length;i++)
+       {
+           if(arr[i]<arr[i-1])
+           {
+              return false;
+
+           }
+       }
+       return true;
+    }
+
+    public static boolean check_arr_asc_desc(int[] arr)
+    {
+        boolean asc = true;
+        boolean desc = true;
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i]<arr[i-1])
+            {
+                asc = false;
+            }
+            else if(arr[i]>arr[i-1])
+            {
+                desc = false;
+            }
+        }
+
+        return asc || desc;
+    }
+    public static void main(String[] args)
+    {
+        int[] arr = {1,2,3,4,5};
+        boolean flag = check_arr_sorted(arr);
+        System.out.println("Flag is"+flag);
+        boolean flag1 = check_arr_asc_desc(arr);
+        System.out.println("Flag is"+flag1);
+
+    }
 }
---
+++ src/Arrays/Easy/Revision/remove_duplicates_sorted_array.java
@@ -0,0 +1,4 @@
+package Arrays.Easy.Revision;
+
+public class remove_duplicates_sorted_array {
+}
